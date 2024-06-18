48ffc8a3a14273550e80bc1bfe2a8f29ced643cd
bravemaster <noumonvikoffidodji@hotmail.fr>
2024-06-18 06:49:28 +0200

    Add

--- utils/redis.js
+++ utils/redis.js
@@ -1,56 +1,38 @@
-#!/usr/bin/node
-
-const redis = require('redis');
-const { promisify } = require('util');
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', (err) => console.error('Redis Client Error:', err));
-    this.connected = false;
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
     this.client.on('connect', () => {
-      this.connected = true;
+      // console.log('Redis client connected to the server');
     });
   }
 
   isAlive() {
-    return this.connected;
+    return this.client.connected;
   }
 
   async get(key) {
-    const getAsync = promisify(this.client.get).bind(this.client);
-    try {
-      const val = await getAsync(key);
-      return val;
-    } catch (err) {
-      console.error('Error getting key:', key, err);
-      throw err;
-    }
+    const value = await this.getAsync(key);
+    return value;
   }
 
   async set(key, value, duration) {
-    const setexAsync = promisify(this.client.setex).bind(this.client);
-    try {
-      const reply = await setexAsync(key, duration, value);
-      return reply;
-    } catch (err) {
-      console.error('Error setting key:', key, err);
-      throw err;
-    }
+    this.client.setex(key, duration, value);
   }
 
   async del(key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    try {
-      const reply = await delAsync(key);
-      return reply;
-    } catch (err) {
-      console.error('Error deleting key:', key, err);
-      throw err;
-    }
+    this.client.del(key);
   }
 }
 
 const redisClient = new RedisClient();
 
-module.exports = redisClient;
+export default redisClient;
