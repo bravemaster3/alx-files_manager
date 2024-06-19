f4a1289bb3a4ec91ff10459654dbe96c8133c772
bravemaster <noumonvikoffidodji@hotmail.fr>
2024-06-19 03:45:35 +0200

    Add

---
+++ controllers/UsersController.js
@@ -0,0 +1,34 @@
+// controllers/UsersController.js
+
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userCollection = dbClient.db.collection('users');
+    const existingUser = await userCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = { email, password: hashedPassword };
+
+    const result = await userCollection.insertOne(newUser);
+
+    return res.status(201).json({ id: result.insertedId, email: newUser.email });
+  }
+}
+
+export default UsersController;
